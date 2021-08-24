d8b19d675a3f2d820ab5d87134c5c6f1063353c6
Hoss <carl.hostrander00@gmail.com>
2021-08-23 21:48:11 -0500

    initial structure of files

---
+++ src/test/java/reftrakker/mn/ReftrakkerMnTest.java
@@ -0,0 +1,21 @@
+package reftrakker.mn;
+
+import io.micronaut.runtime.EmbeddedApplication;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+import jakarta.inject.Inject;
+
+@MicronautTest
+class ReftrakkerMnTest {
+
+    @Inject
+    EmbeddedApplication<?> application;
+
+    @Test
+    void testItWorks() {
+        Assertions.assertTrue(application.isRunning());
+    }
+
+}
